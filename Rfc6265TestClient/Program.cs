7ee5678712320bc02634c9b0e8071e409469873e
Anton Firszov <antonfir@gmail.com>
2020-07-21 00:58:48 +0200

    domain-only test

--- Rfc6265TestClient/Program.cs
+++ Rfc6265TestClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -8,9 +9,15 @@ namespace Rfc6265TestClient
     {
         static async Task Main(string[] args)
         {
-            using HttpClient client = new HttpClient();
-            string result = await client.GetStringAsync("http://localhost:5000");
-            Console.WriteLine(result);
+            HttpClientHandler handler = new HttpClientHandler();
+            CookieContainer cookies = handler.CookieContainer;
+            using HttpClient client = new HttpClient(handler);
+            HttpResponseMessage response = await client.GetAsync("http://localhost:5000");
+
+            foreach (Cookie cookie in cookies)
+            {
+                Console.WriteLine(cookie.ToString());
+            }
         }
     }
 }
